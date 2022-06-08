a8d3588650fc66b31d321ced45d882bf776bdec4
Siddharth Kannan <mail@siddharthkannan.in>
2022-06-08 16:11:13 +0900

    Respond with empty response when the domain is being blocked
    Blocker code is still only a list of hardcoded domains.
    
    	$ dig @localhost -p 8053 fb.com
    	;; Warning: query response not set
    
    	; <<>> DiG 9.11.3-1ubuntu1.17-Ubuntu <<>> @localhost -p 8053 fb.com
    	; (1 server found)
    	;; global options: +cmd
    	;; Got answer:
    	;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 8669
    	;; flags: rd ad; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1
    	;; WARNING: recursion requested but not available
    
    	;; OPT PSEUDOSECTION:
    	; EDNS: version: 0, flags:; udp: 4096
    	; COOKIE: af6155ddc9caf6ea (echoed)
    	;; QUESTION SECTION:
    	;fb.com.				IN	A
    
    	;; ANSWER SECTION:
    	fb.com.			2	IN	A	0.0.0.0
    
    	;; Query time: 0 msec
    	;; SERVER: 127.0.0.1#8053(127.0.0.1)
    	;; WHEN: Wed Jun 08 16:11:19 JST 2022
    	;; MSG SIZE  rcvd: 69
    
    Look into the warning section at the top of this response.

--- setup.go
+++ setup.go
@@ -2,7 +2,7 @@ package blocker
 
 import (
 	"context"
-	"fmt"
+	"net"
 
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
@@ -19,7 +19,10 @@ func init() {
 func setup(c *caddy.Controller) error {
 	// Add the Plugin to CoreDNS, so Servers can use it in their plugin chain.
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		return Blocker{Next: next}
+		return Blocker{
+			Next:    next,
+			Decider: DefaultBlockDomainsDecider{},
+		}
 	})
 
 	// All OK, return a nil error.
@@ -27,15 +30,62 @@ func setup(c *caddy.Controller) error {
 }
 
 type Blocker struct {
-	Next plugin.Handler
+	Next    plugin.Handler
+	Decider BlockDomainsDecider
+}
+
+type BlockDomainsDecider interface {
+	IsDomainBlocked(domain string, questionType uint16) bool
 }
 
 func (b Blocker) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	fmt.Print("serving DNS")
+	if len(r.Question) == 0 {
+		// If the query has no question, then we can't do anything with it here. So, I will just
+		// pass it to the next plugin.
+		return b.Next.ServeDNS(ctx, w, r)
+	}
+
+	question := r.Question[0]
+
+	domain := question.Name
+	questionType := question.Qtype
+
+	// If the domain is blocked, then reply right now with the answer 0.0.0.0
+	if b.Decider.IsDomainBlocked(domain, questionType) {
+		// TODO: Count up a metric here for blocked domains
+
+		emptyAnswer := &dns.A{
+			Hdr: dns.RR_Header{
+				Name:   domain,
+				Ttl:    2,
+				Class:  dns.ClassINET,
+				Rrtype: questionType,
+			},
+			A: net.IP([]byte{0, 0, 0, 0}),
+		}
+
+		r.Answer = []dns.RR{emptyAnswer}
+		w.WriteMsg(r)
+		return dns.RcodeSuccess, nil
+	}
+
 	return b.Next.ServeDNS(ctx, w, r)
 }
 
 // Name ...
 func (b Blocker) Name() string {
 	return PluginName
+}
+
+type DefaultBlockDomainsDecider struct {
+	blockedDomains []string
+}
+
+// IsBlocked ...
+func (d DefaultBlockDomainsDecider) IsDomainBlocked(domain string, questionType uint16) bool {
+	if questionType != dns.TypeA {
+		return false
+	}
+
+	return domain == "fb.com."
 }
