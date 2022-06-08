d4eaf777ee59cefc1d3aa9a69b6f62dbcc585874
Siddharth Kannan <mail@siddharthkannan.in>
2022-06-08 17:07:49 +0900

    Prepare a script which can be run to get the full list of blocked domains

---
+++ blocklist-file-preparer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+wget -O list.oisd.nl --header 'accept: text/plain' https://hosts.oisd.nl
+wget -O list.stevenblack.hosts --header 'accept: test/plain' https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+rm -f list.blocklist-in-full
+cat list.oisd.nl list.stevenblack.hosts | rg -v '^(\s+)?#' | sort -u | rg '^0.0.0.0' > list.blocklist-in-full
+rm list.oisd.nl list.stevenblack.hosts
