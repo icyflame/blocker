29693a839f5a81b6fed9a5973520112f635ae554
Siddharth Kannan <mail@siddharthkannan.in>
2023-02-23 23:17:09 +0900

    Block list must be fetched in the hosts format

--- blocklist-file-preparer.sh
+++ blocklist-file-preparer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-wget -O list.oisd.nl --header 'accept: text/plain' https://big.oisd.nl
+wget -O list.oisd.nl --header 'accept: text/plain' https://big.oisd.nl/hosts
 wget -O list.stevenblack.hosts --header 'accept: test/plain' https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 rm -f list.blocklist-in-full
 cat list.oisd.nl list.stevenblack.hosts | rg -v '^(\s+)?#' | sort -u | rg '^0.0.0.0' > list.blocklist-in-full
