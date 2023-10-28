7082de02f1af9249af2df59d4e06ced818d67369
Siddharth Kannan <mail@siddharthkannan.in>
2023-10-28 20:34:44 +0900

    Provide output file as an argument to the script

--- blocklist-file-preparer.sh
+++ blocklist-file-preparer.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env sh
 
+OUTFILE=$1
+if [[ -z "$OUTFILE" ]];
+then
+	cat <<EOF
+ERROR: Must provide 1 argument, which is the output file
+
+Example:
+
+    $0 ~/list.blocklist-in-full
+EOF
+	exit 43
+fi
+
 wget -O list.oisd.nl --header 'accept: text/plain' https://big.oisd.nl/hosts
 wget -O list.stevenblack.hosts --header 'accept: test/plain' https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-
 
 echo "Hosts from OISD.NL: $(wc -l list.oisd.nl)"
 rm -f list.oisd.nl.cleaned
@@ -20,5 +32,6 @@ rm list.stevenblack.hosts
 
 rm -f list.blocklist-in-full
 sort -u list.oisd.nl.cleaned list.stevenblack.hosts.cleaned > list.blocklist-in-full
+mv list.blocklist-in-full $OUTFILE
 
 rm list.oisd.nl.cleaned list.stevenblack.hosts.cleaned
