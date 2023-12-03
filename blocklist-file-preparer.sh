a2e7e8c65c537a36c6f9ac497e7107c71064c845
Siddharth Kannan <mail@siddharthkannan.in>
2023-12-03 11:29:08 +0900

    Update preparation script to output abp style blocklist

--- blocklist-file-preparer.sh
+++ blocklist-file-preparer.sh
@@ -13,25 +13,19 @@ EOF
 	exit 43
 fi
 
-wget -O list.oisd.nl --header 'accept: text/plain' https://big.oisd.nl/hosts
+wget -O list.oisd.nl.abp --header 'accept: text/plain' https://big.oisd.nl
+echo "Hosts from oisd big: $(wc -l list.oisd.nl.abp)"
+
+# This list is not offered in the ABP format. It blocks some domains which are already blocked by
+# the Big OISD list. However, I am not sure how many of the domains in the StevenBlack list are
+# blocked by OISD. I have not calculated this yet. So, for now, I will continue to merge this list
+# into the OISD list.
 wget -O list.stevenblack.hosts --header 'accept: test/plain' https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-
-echo "Hosts from OISD.NL: $(wc -l list.oisd.nl)"
-rm -f list.oisd.nl.cleaned
-cat list.oisd.nl | rg '^0.0.0.0' > list.oisd.nl.cleaned
-echo >> list.oisd.nl.cleaned
-echo "Hosts from OISD.NL (after cleanup): $(wc -l list.oisd.nl.cleaned)"
-rm list.oisd.nl
-
-echo "Hosts from stevenblack: $(wc -l list.stevenblack.hosts)"
 rm -f list.stevenblack.hosts.cleaned
-cat list.stevenblack.hosts | rg '^0.0.0.0' > list.stevenblack.hosts.cleaned
-echo >> list.stevenblack.hosts.cleaned
-echo "Hosts from stevenblack (after cleanup): $(wc -l list.stevenblack.hosts.cleaned)"
-rm list.stevenblack.hosts
+cat list.stevenblack.hosts | rg '^0.0.0.0' | sed 's#0.0.0.0 ##g' | sed 's#^#||#g' | sed 's#$#^#g' > list.stevenblack.abp
+echo "Hosts from stevenblack: $(wc -l list.stevenblack.abp)"
 
-rm -f list.blocklist-in-full
-sort -u list.oisd.nl.cleaned list.stevenblack.hosts.cleaned > list.blocklist-in-full
+cat list.oisd.nl.abp list.stevenblack.abp > list.blocklist-in-full
 mv list.blocklist-in-full $OUTFILE
 
-rm list.oisd.nl.cleaned list.stevenblack.hosts.cleaned
+rm -f list.oisd.nl.abp list.stevenblack.abp list.stevenblack.hosts
