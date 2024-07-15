006c38ac160541772481bae0dd67b38b704f2316
Siddharth Kannan <mail@siddharthkannan.in>
2024-07-15 20:03:43 +0900

    Print better error strings when something goes wrong

--- setup.go
+++ setup.go
@@ -1,6 +1,8 @@
 package blocker
 
 import (
+	"fmt"
+
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
@@ -24,26 +26,26 @@ func setup(c *caddy.Controller) error {
 	if len(args) < RequiredArgs {
 		// Any errors returned from this setup function should be wrapped with plugin.Error, so we
 		// can present a slightly nicer error message to the user.
-		return plugin.Error(PluginName, c.ArgErr())
+		return plugin.Error(PluginName, c.Errf("number of arguments is less than the required count: given: %d, required: %d", len(args), RequiredArgs))
 	}
 
 	blocklistFilePath := args[0]
 	blocklistUpdateFrequency := args[1]
 	blocklistType := args[2]
 	if blocklistType != string(BlocklistType_Hosts) && blocklistType != string(BlocklistType_ABP) {
-		return plugin.Error(PluginName, c.ArgErr())
+		return plugin.Error(PluginName, c.Errf("blocklist type is not one of the allowed values: %s, %s", BlocklistType_Hosts, BlocklistType_ABP))
 	}
 
 	blocklistResponseType := args[3]
 	if blocklistResponseType != string(ResponseType_Empty) && blocklistResponseType != string(ResponseType_NXDOMAIN) {
-		return plugin.Error(PluginName, c.ArgErr())
+		return plugin.Error(PluginName, c.Errf("blocklist response type is not one of the allowed values: %s, %s", ResponseType_Empty, ResponseType_NXDOMAIN))
 	}
 
 	logger := clog.NewWithPlugin(PluginName)
 
 	decider, shutdownHooks, err := PrepareBlocklist(blocklistFilePath, blocklistUpdateFrequency, blocklistType, logger)
 	if err != nil {
-		return plugin.Error(PluginName, err)
+		return plugin.Error(PluginName, fmt.Errorf("could not prepare blocklist in memory > %w", err))
 	}
 
 	for _, hook := range shutdownHooks {
